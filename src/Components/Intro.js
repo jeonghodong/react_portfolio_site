795b021cbd6a271c3c2bf8f1a345ef4adb371d6f
Hodong <fire13764@gmail.com>
2023-01-08 17:12:43 +0900

    intro 틀 구성중

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -3,7 +3,7 @@ import { SkillData, ContactData } from "../Data/IntroData";
 import TitleBox from "./TitleBox";
 
 const Wrap = styled.div`
-  background-color: rgb(214, 214, 214);
+  background-color: rgb(248 248 248);
   height: 100vh;
   width: 100%;
   margin: 0 auto;
@@ -19,21 +19,27 @@ const Left = styled.div`
   box-shadow: 21px 25px 18px -2px rgba(0, 0, 0, 0.44);
   z-index: 1;
 `;
-
 // right
 const Right = styled.div`
-  background-color: rgb(214, 214, 214);
+  background-color: rgb(248 248 248);
   width: 70%;
-  padding-top: 10rem;
+  padding-top: 130px;
 `;
 const About = styled.h2`
+  position: relative;
   font-size: 3vw;
   font-weight: bold;
   margin-bottom: 5rem;
 `;
-const Skills = styled.h2`
-  font-size: 2vw;
-  font-weight: bold;
+const AboutLine = styled.div`
+  position: absolute;
+  top: 55%;
+  transform: translateY(-50%);
+  width: 70%;
+  height: 5px;
+  background-color: black;
+  margin-left: 1rem;
+  display: inline-block;
 `;
 const Icon = styled.span`
   display: inline-block;
@@ -47,13 +53,27 @@ const SkillBox = styled.div`
     list-style: inside;
   }
 `;
-const SkillName = styled.h2`
-  font-size: 1vw;
+const H2 = styled.h2`
+  font-size: 2vw;
   font-weight: bold;
+  margin-bottom: 3rem;
+`;
+const SkillName = styled.h2`
+  font-size: 1.5vw;
+  font-weight: bold;
+  margin-bottom: 2rem;
 `;
 const ContactBox = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+`;
+const SkillsUl = styled.ul`
+  font-size: 1.3vw;
+  font-weight: 300;
+  margin-bottom: 10rem;
+`;
+const SkillsLi = styled.li`
+  margin-bottom: 3rem;
 `;
 
 function Intro() {
@@ -63,29 +83,33 @@ function Intro() {
         <TitleBox />
       </Left>
       <Right>
-        <About>About me</About>
-        <Skills>Skills</Skills>
+        <About>
+          About me<AboutLine> </AboutLine>
+        </About>
+        <H2>Skills</H2>
         <SkillBox>
           {SkillData.map((v) => (
             <div>
               <SkillName>{v.name}</SkillName>
-              <ul>
+              <SkillsUl>
                 {v.skills.map((v2) => (
-                  <li key={v2}>{v2}</li>
+                  <SkillsLi key={v2}>{v2}</SkillsLi>
                 ))}
-              </ul>
+              </SkillsUl>
             </div>
           ))}
         </SkillBox>
-        <Skills>Contact & Channel</Skills>
-        <ContactBox>
-          {ContactData.map((v) => (
-            <div>
-              <Icon>{v.icon}</Icon>
-              <span>{v.value}</span>
-            </div>
-          ))}
-        </ContactBox>
+        <div>
+          <H2>Contact & Channel</H2>
+          <ContactBox>
+            {ContactData.map((v) => (
+              <div>
+                <Icon>{v.icon}</Icon>
+                <span>{v.value}</span>
+              </div>
+            ))}
+          </ContactBox>
+        </div>
       </Right>
     </Wrap>
   );
