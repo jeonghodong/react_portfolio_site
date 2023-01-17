57528669047839f77c93d946d7bd2e3e853fc717
hodong <fire13764@gmail.com>
2023-01-17 15:43:34 +0900

    rocket button 세부수정

--- src/Components/RocketButton.js
+++ src/Components/RocketButton.js
@@ -1,15 +1,28 @@
 import { forwardRef } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import PropTypes from "prop-types";
+
+const upDown = keyframes`
+0%{
+  bottom: 5rem;
+}
+50%{
+  bottom: 3.5rem;
+}
+100%{
+  bottom: 5rem;
+}
+`;
 
 const TopButton = styled.i`
   color: #0000c5;
-  position: fixed;
+  position: sticky;
   bottom: 5rem;
-  right: 5rem;
+  left: 93vw;
   z-index: 100;
   font-size: 2.5vw;
   cursor: pointer;
+  animation: ${upDown} 2.8s ease-in-out infinite;
 `;
 
 const RocketButton = forwardRef(({ rocketClick }, rocket) => {
